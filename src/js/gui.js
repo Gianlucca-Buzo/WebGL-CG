d593c1b2f46071626252424208e874c5c40bb8f5
Gianluccagit config --global user.name Gianluccagit config --global user.name Gianlucca <gdmbuzo@inf.ufpel.edu.br>
2021-10-04 17:37:29 -0300

    Adicionando controle de tempo e corrigindo bugs na animacao

--- src/js/gui.js
+++ src/js/gui.js
@@ -2,19 +2,20 @@ var config = { rotate: degToRad(20), teste: teste(1), translacaoX: translacaoX(0
 var cameraConfig = { zoom : zoom(50), rotacaoCameraX: rotacaoCameraX(0), rotacaoCameraY: rotacaoCameraY(0) };
 var animationConfig = {firstAnimationType: "", firstAnimationValue: firstAnimationValue(0),startAnimation: false,secondAnimationType: "", secondAnimationValue: secondAnimationValue(0)}
 animationConfig.startAnimation = async function(){
-  executeAnimation(config,animationConfig.firstAnimationType,animationConfig.firstAnimationValue);
+  await executeAnimation(config,animationConfig.firstAnimationType,animationConfig.firstAnimationValue);
   executeAnimation(config,animationConfig.secondAnimationType,animationConfig.secondAnimationValue);
+  return;
 }
 
-options = {rotate: "rotate",rotateX: "rotateX", teste: "teste"}
+options = {rotate: "rotate",translacaoX: "translacaoX", teste: "teste"}
 
 const loadGUI = () => {
   const gui = new dat.GUI();
   var camera = gui.addFolder("Camera")
   camera.add(config, "rotate", 0, 20, 0.5);
   camera.add(config, "teste",-1000,1000,1);
-  camera.add(config, "translacaoX",-1000,1000,1);
-  camera.add(config, "translacaoY",-1000,1000,1);
+  camera.add(config, "translacaoX",-1000,1000,0);
+  camera.add(config, "translacaoY",-1000,1000,0);
   gui.add(cameraConfig, "rotacaoCameraX",-100,100,1);
   gui.add(cameraConfig, "rotacaoCameraY",-100,100,1);
   gui.add(cameraConfig,"zoom",0,120,1);
@@ -29,20 +30,23 @@ const loadGUI = () => {
 async function executeAnimation(config,type,value){
   console.log(type);
   var i = 0
+  var sleepTime = 5000/value
   switch(type){
       case "rotate":
-        for(i = 0; i<value; i++){
-          await sleep(20);
+        for(i = config.rotate; i< (config.rotate + value); i++){
+          await sleep(sleepTime);
           config.rotate = degToRad(i);
-          console.log(config.rotate);
         }
+        break;
       case "translacaoX":
-        for(i = 0; i<value; i++){
-          await sleep(20);
+        var final_value = config.translacaoX + value
+        for(i = config.translacaoX; i< final_value; i++){
+          await sleep(sleepTime);
           config.translacaoX = translacaoX(i);
-          console.log(config.translacaoX);
         }
+        break;
   }
+  return;
 }
 
 
